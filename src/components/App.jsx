31a4a675c8ee4fba5ab59d83789cc4a28cd32cd9
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-07-05 15:38:15 +0300

    refactored code from classes to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm.jsx';
+import { ContactList } from './ContactList/ContactList.jsx';
+import { Filter } from './Filter/Filter.jsx';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const formSubmitHandler = data => {
+    console.log(data);
+    setContacts([...contacts, { ...data, id: nanoid() }]);
+  };
+
+  const changeFilter = ({ target }) => {
+    setFilter(target.value);
+  };
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const handleDelete = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
+
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        width: '300px',
+        padding: '15px',
+        margin: 'auto',
+        alignContent: 'center',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={formSubmitHandler} contacts={contacts} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      {getFilteredContacts().length > 0 && (
+        <ContactList
+          contacts={getFilteredContacts()}
+          handleDelete={handleDelete}
+        />
+      )}
     </div>
   );
 };
